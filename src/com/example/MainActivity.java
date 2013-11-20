78745fc38cbc29a22f78c30386e61fd6597773b8
yinghuihong <123997654@qq.com>
2013-11-20 17:49:16 +0800

    Async

--- src/com/example/MainActivity.java
+++ src/com/example/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example;
 
 import android.app.Activity;
@@ -9,59 +10,69 @@ import android.widget.TextView;
 import com.example.constant.Result;
 import com.example.response.GeocodeResBean;
 import com.example.util.AccessApiUtil;
+import com.open.jsonhttp.AsyncTask;
 import com.open.jsonhttp.OnRequestHttpListener;
 import com.open.jsonhttp.ResponseBean;
 
 public class MainActivity extends Activity implements OnClickListener,
-		OnRequestHttpListener {
+        OnRequestHttpListener {
 
-	private TextView tvHelloWorld;
+    private TextView tvHelloWorld;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		tvHelloWorld = (TextView) findViewById(R.id.tvHelloWorld);
-		findViewById(R.id.btnObtain).setOnClickListener(this);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvHelloWorld = (TextView) findViewById(R.id.tvHelloWorld);
+        findViewById(R.id.btnObtain).setOnClickListener(this);
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btnObtain:
-			AccessApiUtil.gencode("HK", this);
-			break;
+    AsyncTask mTaskGenCode;
 
-		default:
-			break;
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnObtain:
+                mTaskGenCode = AccessApiUtil.gencode("HK", this);
+                break;
 
-	@Override
-	public void onSuccess(int responseKey, ResponseBean responseBean) {
-		switch (responseKey) {
-		case AccessApiUtil.INDEX_GEOCODE:
-			if (responseBean.getStatus().equals(Result.OK)) {
-				GeocodeResBean resBean = (GeocodeResBean) responseBean;
-				tvHelloWorld.setText(resBean.toString());
-			}
-			break;
+            default:
+                break;
+        }
+    }
 
-		default:
-			break;
-		}
-	}
+    @Override
+    public void onSuccess(int responseKey, ResponseBean responseBean) {
+        switch (responseKey) {
+            case AccessApiUtil.INDEX_GEOCODE:
+                if (responseBean.getStatus().equals(Result.OK)) {
+                    GeocodeResBean resBean = (GeocodeResBean) responseBean;
+                    tvHelloWorld.setText(resBean.toString());
+                }
+                break;
 
-	@Override
-	public void onFail(int responseKey, int code) {
-		switch (responseKey) {
-		case AccessApiUtil.INDEX_GEOCODE:
+            default:
+                break;
+        }
+    }
 
-			break;
+    @Override
+    public void onFail(int responseKey, int code) {
+        switch (responseKey) {
+            case AccessApiUtil.INDEX_GEOCODE:
 
-		default:
-			break;
-		}
-	}
+                break;
 
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTaskGenCode != null) {
+            mTaskGenCode.interrupt();
+        }
+        super.onDestroy();
+    }
 }
